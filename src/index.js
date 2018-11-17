574679777d0aba0d7f84cf2438e70de18f3be77a
Malaev <potap99122@gmail.com>
2018-11-17 12:28:15 +0500

    login

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import axios from 'axios';
 
 const url = 'http://10.155.57.152:8080';
 
-App({ name: 'Example Name ', email: 'username@example.com'})
-
-// axios.get(url + '/account/info', { headers: { authorization: localStorage.getItem('secretKey') }})
-//     .then(() => {
-//         console.log('app');
-//         App({ name: 'Example Name ', email: 'username@example.com'})
-//     })
-//     .catch(() => {
-//         console.log('login');
-//         App({ name: 'Name ', email: 'username@example.com' });
-//     });
+axios.get(url + '/account/info', { headers: { authorization: localStorage.getItem('secretKey') }})
+    .then((res) => {
+        console.log(res);
+        App({ name: 'Example Name ', email: 'username@example.com'})
+    })
+    .catch(() => {
+        console.log('login');
+        Login({ name: 'Name ', email: 'username@example.com' });
+    });
