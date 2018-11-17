358edd078d5b55b5fe0b306d7f33b93cf2c88d45
Evgenii Shumeiko <nicesay.ru@gmail.com>
2018-11-17 21:27:40 +0500

    okay

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@
 import { default as App } from "@app";
 import { default as Chat } from "./modules/chat";
 import { default as Login } from "./modules/login";
-import { default as Mentor } from './modules/Mentor'
-import axios from 'axios';
+import { default as Mentor } from "./modules/Mentor";
+import axios from "axios";
 
-export const url = 'https://4c8e7c34.ngrok.io';
+export const url = "https://4c8e7c34.ngrok.io";
 
-axios.get(url + '/account/role', { headers: { authorization: localStorage.getItem('secretKey') }})
-    .then((res) => {
+axios
+    .get(`${url}/account/role`, { headers: { authorization: localStorage.getItem("secretKey") } })
+    .then(res => {
         if (window.location.pathname.indexOf("chat") === -1) {
-            if (res.data === 'developer')
-                App({name: 'Example Name ', email: 'username@example.com'})
-            else if (res.data === 'mentor')
-                Mentor({name: 'Example Name ', email: 'username@example.com'})
-        }
-        else
-            Chat(res)
-    });
+            if (res.data === "developer") App({ name: "Example Name ", email: "username@example.com" });
+            else if (res.data === "mentor") Mentor({ name: "Example Name ", email: "username@example.com" });
+        } else Chat(res);
+    })
+    .catch(() => {});
