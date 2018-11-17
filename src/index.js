40207a14fbc6e74c970aee62b96ea851e5faf7ac
VitaliyDm <vitaldmitrie@gmail.com>
2018-11-18 03:42:04 +0500

    merge conflicts

--- src/index.js
+++ src/index.js
@@ -7,22 +7,21 @@ import axios from "axios";
 import environment from '@core/environment.json';
 const url = environment.API.HOST;
 
-App({user: 'user', email: 'email'});
-//axios
-//    .get(`${url}/account/role`, { headers: { authorization: localStorage.getItem("secretKey") } })
-//    .then(res => {
-//        console.log(res);
-//        console.log(url);
-//        if (window.location.pathname.indexOf("chat") === -1) {
-//            if (res.data === "developer")//
-//                App({ name: "Example Name ", email: "username@example.com" })
-//            else if (res.data === "reporter")
-//                Mentor({ name: "Example Name ", email: "username@example.com" })
-//            else if (res.data === "reporter")
-//                Login({ name: "Name ", email: "username@example.com" });
-//        } else Chat(res);
-//    })
-//    .catch(() => {
-//        console.log("login");
-//        Login({ name: "Name ", email: "username@example.com" });
-//    });
+axios
+    .get(`${url}/account/role`, { headers: { authorization: localStorage.getItem("secretKey") } })
+    .then(res => {
+        console.log(res);
+        console.log(url);
+        if (window.location.pathname.indexOf("chat") === -1) {
+            if (res.data === "developer")//
+                App({ name: "Example Name ", email: "username@example.com" })
+            else if (res.data === "reporter")
+                Mentor({ name: "Example Name ", email: "username@example.com" })
+            else if (res.data === "reporter")
+                Login({ name: "Name ", email: "username@example.com" });
+        } else Chat(res);
+    })
+    .catch(() => {
+        console.log("login");
+        Login({ name: "Name ", email: "username@example.com" });
+    });
